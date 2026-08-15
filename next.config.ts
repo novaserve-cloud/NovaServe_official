d8d4a1fe58b4b72c3d8fdc5c5769766a26b0d231
Md Shadab Azam ANsari <shadabazama@gmail.com>
2026-08-16 05:12:52 +0530

    chore: setup github pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,40 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   reactStrictMode: true,
   images: {
     unoptimized: true,
-  },
-  compress: true,
-  poweredByHeader: false,
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "X-DNS-Prefetch-Control",
-            value: "on",
-          },
-          {
-            key: "Strict-Transport-Security",
-            value: "max-age=63072000; includeSubDomains; preload",
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
-            key: "Referrer-Policy",
-            value: "origin-when-cross-origin",
-          },
-        ],
-      },
-    ];
   },
 };
 
